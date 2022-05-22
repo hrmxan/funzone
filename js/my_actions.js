6e48966bc8bd9c1719ed32d60889e69eed198f69
HRMxan <hrmxan@gmail.com>
2022-05-22 18:45:32 +0500

    only footer

--- js/my_actions.js
+++ js/my_actions.js
@@ -1,8 +1,8 @@
 let colapsBar = document.getElementById('colapsBar');
 let mute_buttons = document.querySelectorAll('.mute_buttons');
 mute_buttons.forEach(function(item){
-    item.addEventListener('click', () => {
-        item.preventDefault;
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
     });
 });
 let burgerButton = document.getElementById('burgerButton');
@@ -12,4 +12,15 @@ burgerButton.addEventListener('click', () => {
 let closeNav = document.getElementById('closeNav');
 closeNav.addEventListener('click', () => {
     colapsBar.classList.remove('colapsed');
+});
+
+let tips__nav_item = document.querySelectorAll('.tips__nav_item');
+tips__nav_item.forEach(item => {
+    item.addEventListener('click', () => {
+        tips__nav_item.forEach(e => {
+            e.classList.remove('active');
+        }
+        );
+        item.classList.add('active');
+    });
 });
