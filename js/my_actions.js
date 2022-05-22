ae87dde222febe7ac59f112fa4213da06b3a313f
HRMxan <hrmxan@gmail.com>
2022-05-22 06:58:24 +0500

    kip up

--- js/my_actions.js
+++ js/my_actions.js
@@ -2,7 +2,7 @@ let colapsBar = document.getElementById('colapsBar');
 let mute_buttons = document.querySelectorAll('.mute_buttons');
 mute_buttons.forEach(function(item){
     item.addEventListener('click', () => {
-        item.preventDefault();
+        item.preventDefault;
     });
 });
 let burgerButton = document.getElementById('burgerButton');
