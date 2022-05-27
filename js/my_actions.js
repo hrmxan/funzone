d74492fe223466b0a07f68485d80ba46597b068d
HRMxan <hrmxan@gmail.com>
2022-05-27 11:58:21 +0500

    reservation

--- js/my_actions.js
+++ js/my_actions.js
@@ -195,10 +195,10 @@ try {
   filter_select.forEach(item => {
     item.addEventListener('click', () => {
       filter_select.forEach(e => {
-        if (e!=item) e.classList.remove('drobon');
+        if (e != item) e.classList.remove('drobon');
       });
       item.classList.toggle('drobon');
-      
+
     });
   });
 } catch (error) {
@@ -220,6 +220,119 @@ try {
       prevEl: ".swiper-button-prev",
     },
   });
+} catch (error) {
+  console.log(error);
+}
+
+
+// calendar in sport
+
+
+try {
+  document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'timeGridWeek',
+      titleFormat: { // will produce something like "Tuesday, September 18, 2018"
+        month: 'long',
+        year: 'numeric',
+        day: 'numeric',
+        weekday: 'long'
+      },
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      initialDate: '2020-09-12',
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      select: function (arg) {
+        var title = prompt('Event Title:');
+        if (title) {
+          calendar.addEvent({
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+          })
+        }
+        calendar.unselect()
+      },
+      eventClick: function (arg) {
+        console.log(arg);
+        if (confirm('Are you sure you want to delete this event?')) {
+          arg.event.remove()
+        }
+      },
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      // eventColor: '#F14141',
+      // eventBackgroundColor: '#37c7564f',
+      // events: [{
+      //     title: 'All Day Event',
+      //     start: '2020-09-01'
+      //   },
+      //   {
+      //     title: 'Long Event',
+      //     start: '2020-09-07',
+      //     end: '2020-09-10'
+      //   },
+      //   {
+      //     groupId: 999,
+      //     title: 'Repeating Event',
+      //     start: '2020-09-09T16:00:00'
+      //   },
+      //   {
+      //     groupId: 999,
+      //     title: 'Repeating Event',
+      //     start: '2020-09-16T16:00:00'
+      //   },
+      //   {
+      //     title: 'Conference',
+      //     start: '2020-09-11',
+      //     end: '2020-09-13'
+      //   },
+      //   {
+      //     title: 'Meeting',
+      //     start: '2020-09-12T10:30:00',
+      //     end: '2020-09-12T12:30:00'
+      //   },
+      //   {
+      //     title: 'Lunch',
+      //     start: '2020-09-12T12:00:00'
+      //   },
+      //   {
+      //     title: 'Meeting',
+      //     start: '2020-09-12T14:30:00'
+      //   },
+      //   {
+      //     title: 'Happy Hour',
+      //     start: '2020-09-12T17:30:00'
+      //   },
+      //   {
+      //     title: 'Dinner',
+      //     start: '2020-09-12T20:00:00'
+      //   },
+      //   {
+      //     title: 'Birthday Party',
+      //     start: '2020-09-13T07:00:00'
+      //   },
+      //   {
+      //     title: 'Click for Google',
+      //     url: 'http://google.com/',
+      //     start: '2020-09-28'
+      //   }
+      // ]
+    });
+
+    console.log(calendar);
+
+    calendar.render();
+  });
+
 } catch (error) {
   console.log(error);
 }
